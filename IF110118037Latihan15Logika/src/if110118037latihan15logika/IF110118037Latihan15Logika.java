a7a03145cef6689fbcced1c3f564e7126fa2b512
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 15:19:46 +0700

    Add files via upload

---
+++ IF110118037Latihan15Logika/src/if110118037latihan15logika/IF110118037Latihan15Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan15logika;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program Logika.
+ */
+public class IF110118037Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " +(a && b));
+        System.out.println("a || b = " +(a || b));
+        System.out.println("!a = " +(!a));
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
